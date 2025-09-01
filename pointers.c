37ac0e791a7b348d782c1261220780b9f2a2cfa8
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2025-09-01 14:19:31 -0400

    initial thing for pointers here

--- pointers.c
+++ pointers.c
@@ -14,22 +14,23 @@ coordinate_t new_coord(int x, int y, int z)
     return c;
 };
 
-coordinate_t scale_coordinate(coordinate_t c, int scale_factor)
+void scale_coordinate(coordinate_t *c, int scale_factor)
 {
-    c.x *= scale_factor;
-    c.y *= scale_factor;
-    c.z *= scale_factor;
-    return c;
+    printf("the pointer to c: %p\n", c);
+    printf("the value at pointer c: %d, %d, %d\n", (*c).x, (*c).y, (*c).z);
+    c->x *= scale_factor; // equivalent to (*c).x
+    (*c).y *= scale_factor;
+    (*c).z *= scale_factor;
 };
 
 int main()
 {
     printf("Hello, World!\n");
     coordinate_t the_call = new_coord(2, 3, 5);
-    coordinate_t the_scaled_call = scale_coordinate(the_call, 3);
+    coordinate_t *the_call_ptr = &the_call;
 
     printf("the_call: x=%d, y=%d, z=%d\n", the_call.x, the_call.y, the_call.z);
-    printf("the_scaled_call: x=%d, y=%d, z=%d\n", the_scaled_call.x, the_scaled_call.y, the_scaled_call.z);
+    scale_coordinate(the_call_ptr, 3);
     printf("post: x=%d, y=%d, z=%d\n", the_call.x, the_call.y, the_call.z);
 
     printf("size of coordinate_t: %lu\n", sizeof(coordinate_t));
